703506e89b6e5744d67a9726f098b737e9a8b700
nachovigilante <nachovigilante@gmail.com>
2022-08-01 01:25:38 -0300

    feat(Header): Changed Header style

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
 import React from "react";
 import styles from "./Header.module.scss";
 
 const Header = ({ switchTheme }: { switchTheme: () => void }) => {
     return (
         <header className={styles["header"]}>
-            <h1>Header</h1>
+            <Link href="/">
+                <a>
+                    <h1>Cheatsheets</h1>
+                </a>
+            </Link>
         </header>
     );
 };

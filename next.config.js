8ecbe3c6fa5e63bacff5c75dadc836c3f2ee6d93
nachovigilante <nachovigilante@gmail.com>
2022-08-02 11:20:49 -0300

    feat(build): Added SVGr support

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            exclude: /node_modules/,
+            use: ["@svgr/webpack"],
+        });
+        return config;
+    },
     reactStrictMode: true,
     swcMinify: true,
 };

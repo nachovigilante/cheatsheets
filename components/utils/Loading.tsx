2b24b1c6e5ca74cbceb43dd323efeabb30e768d5
nachovigilante <nachovigilante@gmail.com>
2022-08-05 02:47:08 -0300

    fix(Loading): Loading now uses next/image

--- components/utils/Loading.tsx
+++ components/utils/Loading.tsx
@@ -1,9 +1,10 @@
 import styles from "./Loading.module.scss";
+import Image from "next/image";
 
 const Loading = () => {
     return (
         <div className={styles["loading"]}>
-            <div className={styles["loading-logo"]} />
+            <Image src="/assets/images/Logo-anim.gif" height="100" width="100" />
             <h1>Cargando...</h1>
         </div>
     );

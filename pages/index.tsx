f4d7ba3fc3e9e2aee3624d253eef72badcef47a7
nachovigilante <nachovigilante@gmail.com>
2022-08-16 12:07:24 -0300

    feat(Home): Added content and style to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import styles from "./index.module.scss";
 import Head from "next/head";
-import Sheet from "../components/utils/Sheet";
 
 const Home: NextPage = () => {
     return (
@@ -11,16 +10,51 @@ const Home: NextPage = () => {
             </Head>
             <h1>Bienvenidos al repositorio de cheatsheets de TIC</h1>
             <p>
-                En esta web se encuentra una colección de &quot;hojas de
-                trucos&quot; a las que se puede recurrir a la hora de programar
-                en cualquiera de los lenguajes disponibles.
+                En esta web se encuentra una colección de{" "}
+                <strong>&quot;hojas de trucos&quot;</strong> a las que se puede
+                recurrir a la hora de programar en cualquiera de los lenguajes
+                disponibles.
             </p>
             <p>
                 La idea es que éste sea de realización colectiva, es decir, que
-                todos (tanto profesores como alumnos) puedan hacer su aporte a
-                los cheatsheets, con el objetivo de aumentar la calidad y
-                cantidad de información.
+                todos (tanto profesores como <strong>alumnos</strong>) puedan
+                hacer su aporte a los cheatsheets, con el objetivo de aumentar
+                la calidad y la cantidad de información.
             </p>
+
+            <h2>Aportes</h2>
+
+            <p>
+                Para aportar algún cambio o un cheatsheet nuevo, es{" "}
+                <strong>muy importante</strong> que leas el{" "}
+                <a
+                    href="https://github.com/nachovigilante/cheatsheets#readme"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    README
+                </a>{" "}
+                del repositorio.
+            </p>
+
+            <h2>Estrellas</h2>
+
+            <p>
+                Si te fue útil este repositorio dale una estrella{" "}
+                <a
+                    href="https://github.com/nachovigilante/cheatsheets"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    acá
+                </a>
+                .
+            </p>
+            <div className={styles["star"]}>
+                <div className={styles["image"]} />
+                <h2> ----&gt; </h2>
+                <div className={styles["image"]} />
+            </div>
         </div>
     );
 };

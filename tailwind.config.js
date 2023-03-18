f09728d4ec8f574e5f6dddd8f08d6d98e0532647
nachovigilante <nachovigilante@gmail.com>
2023-03-18 15:34:35 -0300

    build: added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./styles/**/*.{js,ts,jsx,tsx,scss,css}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};

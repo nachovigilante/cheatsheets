267d3dbc20e77ed54e6a3ca4dea572da3e580968
nachovigilante <nachovigilante@gmail.com>
2023-03-18 18:46:40 -0300

    fix(header): minor style fix

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
     const { pathname } = useRouter();
 
     return (
-        <header className="py-4 pb-14 px-8 flex items-center justify-between fixed z-50 top-0 w-screen backdrop-blur-sm fade-bottom">
+        <header className="py-4 pb-20 px-8 flex items-center justify-between fixed z-50 top-0 w-screen backdrop-blur-sm fade-bottom">
             <Link href="/">
                 <a className="flex items-center no-underline gap-5">
                     <Logo height="45    " />

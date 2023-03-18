60e3cf030c936201276e43ec765c93325f39f314
nachovigilante <nachovigilante@gmail.com>
2023-03-18 18:47:02 -0300

    fix(section): added id to section

--- components/layout/Section.tsx
+++ components/layout/Section.tsx
@@ -1,20 +1,22 @@
 import { twMerge } from "tailwind-merge";
 
 type SectionProps = {
+    id?: string;
     children?: React.ReactNode;
     className?: string;
     row?: boolean;
 };
 
-const Section = ({ children, className, row }: SectionProps) => {
+const Section = ({ children, className, row, id }: SectionProps) => {
     return (
         <section
             className={twMerge(className, "flex justify-center items-center")}
+            id={id}
         >
             <div
                 className={twMerge(
                     "w-[1200px] m-auto flex justify-center items-center",
-                    !row && "flex-col",
+                    !row && "flex-col"
                 )}
             >
                 {children}

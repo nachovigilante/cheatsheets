d92867b4468404b3c6f734fc7984265d6e0676b6
nachovigilante <nachovigilante@gmail.com>
2023-03-28 18:30:50 -0300

    feat(responsive): terminal now looks rigth in xl screens

--- components/utils/Terminal/Terminal.tsx
+++ components/utils/Terminal/Terminal.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { twMerge } from "tailwind-merge";
 import {
     TerminalContext,
@@ -37,35 +37,34 @@ const Window = ({ cheatsheets }: TerminalProps) => {
     return (
         <div
             className={twMerge(
-                "transition-all duration-75 rounded-default border-3 border-[#504e54] bg-[#2322256d] backdrop-blur-xl overflow-hidden w-full h-[600px]",
+                "transition-all duration-75 rounded-default border-3 border-[#504e54] bg-[#2322256d] backdrop-blur-xl overflow-hidden w-full 2xl:h-[600px] h-[500px]",
                 error ? "error" : ""
             )}
         >
-            <div className="bg-[#2a2831] font-space h-20 text-2xl flex items-center border-b-1 border-b-black">
-                <div className="flex gap-3 p-5 self-start">
-                    <div className="bg-[#ff5f56] w-5 h-5 rounded-full" />
-                    <div className="bg-[#ffbd2e] w-5 h-5 rounded-full" />
-                    <div className="bg-[#27c93f] w-5 h-5 rounded-full" />
+            <div className="bg-[#2a2831] font-space 2xl:h-20 h-[70px] text-2xl flex items-center border-b-1 border-b-black">
+                <div className="flex gap-3 2xl:p-5 p-4 self-start">
+                    <div className="bg-[#ff5f56] 2xl:w-5 2xl:h-5 w-4 h-4 rounded-full" />
+                    <div className="bg-[#ffbd2e] 2xl:w-5 2xl:h-5 w-4 h-4 rounded-full" />
+                    <div className="bg-[#27c93f] 2xl:w-5 2xl:h-5 w-4 h-4 rounded-full" />
                 </div>
                 <div className="flex-grow text-center">tic://cheatsheets</div>
             </div>
-            <div className="flex h-[520px]">
-                <div className="bg-[#242229] p-5 font-space">
-                    <div className="text-xl mb-3">
+            <div className="flex 2xl:h-[520px] h-[430px]">
+                <div className="bg-[#242229] p-5 font-space pb-16">
+                    <div className="2xl:text-xl text-lg 2xl:mb-3 mb-2">
                         <i className="fa-solid fa-chevron-down mr-3"></i>
                         CHEATSHEETS
                     </div>
-                    <ul className="text-lg list-none pl-3 pb-16 w-60">
+                    <ul className="list-none w-60 overflow-y-auto pb-10 pl-3 max-h-full fade-bottom-scroll">
                         {cheatsheets.map((c) => (
                             <li
                                 key={c.slug}
-                                className="flex gap-2 items-center text-xl py-1 px-2 hover:bg-[#2a2831] cursor-pointer"
+                                className="flex gap-2 items-center 2xl:text-xl text-lg py-1 px-2 hover:bg-[#2a2831] cursor-pointer"
                             >
                                 <img
                                     src={`/assets/images/${c.slug}.svg`}
                                     alt={`${c.slug} logo`}
-                                    width="18"
-                                    height="18"
+                                    className="h-5 w-5"
                                 />
                                 <span>{c.slug}.md</span>
                             </li>

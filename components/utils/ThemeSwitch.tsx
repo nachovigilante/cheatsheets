04b7eccb5dd724dfc5e7aa3639a260687d54fc53
nachovigilante <nachovigilante@gmail.com>
2023-04-06 15:33:04 -0300

    fix: changed theme switch style

--- components/utils/ThemeSwitch.tsx
+++ components/utils/ThemeSwitch.tsx
@@ -4,7 +4,6 @@ import MoonIcon from "../../public/assets/icons/Moon.svg";
 import SunIcon from "../../public/assets/icons/Sun.svg";
 import { ThemeContext } from "../../contexts/ThemeContext";
 
-import styles from "./ThemeSwitch.module.scss";
 import { twMerge } from "tailwind-merge";
 
 const ThemeSwitch = () => {
@@ -17,37 +16,42 @@ const ThemeSwitch = () => {
                 setTheme((theme) => (theme === "light" ? "dark" : "light"));
             }}
         >
-            <div className="h-6 w-12 rounded-full bg-transparent flex items-center justify-between overflow-hidden shadow-lg shadow-black">
-                <div
+            <div
+                className={twMerge(
+                    theme === "dark" && "border-transparent",
+                    "h-7 w-12 rounded-full bg-transparent flex items-center justify-between overflow-hidden theme-border"
+                )}
+            >
+                {/* <div
                     className={twMerge(
                         theme !== "dark" && "flex-grow-0",
-                        "h-full transition-[flex-grow_0.3s_ease] rounded-[15px_0px_0px_15px] flex-grow bg-dark-doc"
+                        "h-full transition-[flex-grow_0.3s_ease] rounded-[15px] flex-grow bg-doc"
                     )}
                 />
                 <div
                     className={twMerge(
-                        "h-full transition-[flex-grow_0.3s_ease] rounded-[0px_15px_15px_0px] flex-grow bg-doc w-3",
-                        theme === "dark" && "flex-grow-0 w-3"
+                        "h-full transition-[flex-grow_0.3s_ease] rounded-[15px] flex-grow w-3",
+                        theme === "dark" && "flex-grow-0 w-0"
                     )}
-                />
+                /> */}
             </div>
             <div
                 className={twMerge(
-                    "w-7 h-7 rounded-full absolute ml-0 bg-doc transition-[margin_0.3s_ease] flex items-center justify-center shadow-lg shadow-black",
-                    theme === "dark" && "ml-6 bg-dark-doc"
+                    "w-5 h-5 rounded-full absolute ml-1 transition-[margin_0.3s_ease] flex items-center justify-center shadow-lg bg-doc",
+                    theme === "dark" && "ml-6"
                 )}
             >
                 {theme === "dark" ? (
                     <MoonIcon
-                        height="17px"
-                        width="17px"
+                        height="15px"
+                        width="15px"
                         viewBox="0 0 25 25"
-                        fill="#e2e2e2"
+                        fill="#170c60"
                     />
                 ) : (
                     <SunIcon
-                        height="17px"
-                        width="17px"
+                        height="15px"
+                        width="15px"
                         viewBox="1 -1 25 25"
                         fill="#170c60"
                     />

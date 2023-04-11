7210ba0ca1d781c46cb5890f1bfd8a97f8637492
nachovigilante <nachovigilante@gmail.com>
2023-04-11 10:39:11 -0300

    feat(GA): added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider } from "../contexts/ThemeContext";
 import Loading from "../components/utils/Loading";
 import { LoadingProvider } from "../contexts/LoadingContext";
 import Background from "../components/layout/Background";
+import Script from "next/script";
 
 function App({ Component, pageProps }: AppProps) {
     const [theme] = useState(true);
@@ -20,6 +21,21 @@ function App({ Component, pageProps }: AppProps) {
                         content="width=device-width, initial-scale=1"
                     />
                 </Head>
+                <Script
+                    strategy="lazyOnload"
+                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                />
+
+                <Script id="ga" strategy="lazyOnload">
+                    {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+                </Script>
                 <div id="root">
                     <Header />
                     <Component {...pageProps} />
